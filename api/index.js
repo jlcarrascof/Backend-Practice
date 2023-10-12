208abfb2d3b93fc464f013ccf97f2cbb426fa98a
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-11 22:42:56 -0400

    Use Morgan to test my middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-const app = require(".src/app.js");
+const app = require("./src/app");
 
 app.listen(3001, () => { 
     console.log("Server listening on port 3001");
 });
- 
--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    console.log('This is my middleware');
+    next();
+});
 
 module.exports = app;
