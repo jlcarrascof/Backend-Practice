dbe5ec975193dc217cf04cee6afb45504b307947
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-12 01:12:30 -0400

    Adding request for create an user using posts

--- api/src/app.js
+++ api/src/app.js
@@ -22,6 +22,10 @@ app.get('/users/:id', (req, res) => {
     res.status(200).send('Im in /users/:id');
 });
 
+app.post('/users', (req, res) => {
+    res.status(200).send('Im going to create an user');
+});
+
 app.get('/posts', (req, res) => {
     res.status(200).send('Using posts');
 });
